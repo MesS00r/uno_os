0337cc8260d7b6193f54f085965f4f0364987e83
MesS00r <art65734521@gmail.com>
2026-04-05 19:54:56 +0300

    second commit.

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#define F_CPU 16000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <usart.h>
+
+int main(void) {
+    ubegin(9600);
+
+    uprintln0("hello world!!!");
+
+    return 0;
+}
