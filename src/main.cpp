e45df70bde634887bf7e45b4f48b40a0bca2b6a0
MesS00r <art65734521@gmail.com>
2026-04-18 14:14:24 +0300

    uscan is finished.

--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,24 @@
 int main(void) {
     ubegin(9600);
 
-    // uprint("Hello ");
-    // uprintln("world");
+    uprint("Hello ");
+    uprintln("world");
 
     sys_buffer *buf = buffer_init();
-    uset_line(buf, 4, NULL);
+    input_timer_init();
 
-    char word[10] = {0};
+    uset_line(buf);
+
+    char word[8] = {0};
     uread_word(buf, word, sizeof(word));
 
+    uset_line(buf);
+
+    uint16_t num = 0;
+    uread_num(buf, &num);
+
     uprintln(word);
+    uprintln(num);
 
     return 0;
 }
